530daacca45ac5e8d6ac7c45eef8dbaaf40835be
Nikola Najic <nnajic1@gmail.com>
2024-08-29 10:20:06 +0200

    Napravljena mreza i trening fajl sa 200 epoha.

---
+++ main.py
@@ -0,0 +1,32 @@
+from tensorflow.keras.models import load_model # type: ignore
+import tensorflow as tf
+from utilities import *
+import matplotlib.pyplot as plt
+
+def main():
+    # Učitavanje istreniranog modela
+    model = load_model('AI.h5')
+
+    # Učitavanje slika za obradu
+    folder_path = './archive/color'
+    color_images, gray_images = LoadImagesFromFolder(folder_path)
+
+    # Obrada slika
+    num = 10 # broj slika koje ćemo obraditi
+    after_ai = model.predict(gray_images[-num:])
+    
+    # Prikaz obrađenih slika
+    fig, ax = plt.subplots(3, num, figsize = (200, 100))
+    fig.set_label("Rezultati")
+    for i in range(num): 
+        ax[0][i].set_axis_off()
+        ax[1][i].set_axis_off()
+        ax[2][i].set_axis_off()
+        ax[0][i].imshow(color_images[-num+i])
+        ax[1][i].imshow(gray_images[-num+i], cmap = 'gray')
+        ax[2][i].imshow(after_ai[i])
+    fig.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
---
+++ utilities.py
@@ -0,0 +1,54 @@
+import os, cv2, numpy as np
+from tensorflow.keras import layers, models # type: ignore
+
+def LoadImagesFromFolder(folder):
+    color_images, gray_images = [], []
+    for file_name in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, file_name))
+        if img is not None:
+            img = cv2.resize(img, (128, 128))
+            color_images.append(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            gray_images.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+
+    color_images = np.array(color_images)
+    gray_images = np.array(gray_images)
+    gray_images = np.expand_dims(gray_images, axis=-1)
+
+    color_images = color_images / 255.
+    gray_images = gray_images / 255.
+
+    return (color_images, gray_images)
+
+def MakeModel(input_size=(128, 128, 1)):
+    inputs = layers.Input(input_size)
+    
+    # Encoder
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    # Decoder
+    up4 = layers.Conv2DTranspose(128, 2, strides=(2, 2), padding='same')(conv3)
+    merge4 = layers.concatenate([conv2, up4], axis=3)
+    conv4 = layers.Conv2D(128, 3, activation='relu', padding='same')(merge4)
+    conv4 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv4)
+
+    up5 = layers.Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(conv4)
+    merge5 = layers.concatenate([conv1, up5], axis=3)
+    conv5 = layers.Conv2D(64, 3, activation='relu', padding='same')(merge5)
+    conv5 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv5)
+
+    # Output layer
+    outputs = layers.Conv2D(3, 1, activation='sigmoid')(conv5)  # 3 kanala za RGB
+    
+    model = models.Model(inputs=inputs, outputs=outputs)
+    return model
+
